54fff195f2a6cfe478cd9f1ec3ee72cf486dd5cc
mohamed <mohamed97@gmail.com>
2022-06-21 02:15:30 +0200

    complete ReadMe file add comments make responsive navBar

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # Landing Page Project
 
-## Table of Contents
+The code in this repository is meant to satisfy the project # 1 rubric of Professional Front-End Web Development Nanodegree Program.
+Landing page is the conversion of a static page to a page that allows some sort of interaction. The dynamicity is achieved through javascript.
 
-* [Instructions](#instructions)
+___
 
-## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+Starter code taken from udacity [repository](https://github.com/udacity/fend/tree/refresh-2019/projects/landing-page).
 
-To get started, open `js/app.js` and start building out the app's functionality
+This project has the scope of converting a static web page in a single interactive page.
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+It uses javascript to dynamically create navbar links based on the content, when a section is in the viewport it show the active state of that section.
+
+
+During the scrolling the navbar is automatically hidden when user stop scrolling for 2000 mseconds.
+
+
+When scrolling , a new button will appear close to the bottom of the page. This button can be used by the users to scroll to the very top of the page.
+
+Make sections collapsible when pressing (...more) they open and when pressing (...lower) they close.
--- js/app.js
+++ js/app.js
@@ -1,62 +1,64 @@
 /**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
+ * Global variables
  */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
-
 const Sections = document.querySelectorAll("section");
 const mainNavbarList = document.getElementById("navbar__list");
 const mainHeader = document.getElementById("mainHeader");
+let enableScroll = true;
+/* Helper Functions */
 
+/**
+ * @description Activate the anchor and the section
+ * @param {Node} allAnchors - The anchor of header that i need to activate
+ * @param {Node} Sections - The section that i need to activate
+ */
+function activateClasses(allAnchors, Sections) {
+  allAnchors.classList.remove("menu__link");
+  allAnchors.classList.add("active");
+  Sections.classList.add("your-active-class");
+}
+
+/**
+ * @description Deactivate the anchors and the sections
+ * @param {Node} allAnchors - The anchors of header that i need to deactivate
+ * @param {Node} Sections - The sections that i need to deactivate
+ */
+function deactivateClasses(allAnchors, Sections) {
+  allAnchors.classList.remove("active");
+  allAnchors.classList.add("menu__link");
+  Sections.classList.remove("your-active-class");
+}
+
+/**
+ * @description Activate or deactivate the anchor and the section
+ * @param {Node} allAnchors - The anchors of header that i need to activate or deactivate
+ * @param {Number} index - The index of anchor and section that i need to activate
+ */
+function setClassesName(allAnchors, index) {
+  for (let i = 0; i < allAnchors.length; i++) {
+    if (i === index) {
+      activateClasses(allAnchors[i], Sections[i]);
+    } else {
+      deactivateClasses(allAnchors[i], Sections[i]);
+    }
+  }
+}
+
+/**
+ * @description Determine whether the section is visible on the viewport or not
+ * @param {Node} section
+ * @returns boolean value (true or false)
+ */
+function sectionInViewport(section) {
+  const rect = section.getBoundingClientRect();
+  return rect.top >= 0 && rect.bottom <= window.innerHeight;
+}
+
+/* Main Functions */
+
+/**
+ * @description populate navbar links based on the content
+ */
 function populateNavBarMenu() {
   const fragment = document.createDocumentFragment();
   for (const section of Sections) {
@@ -71,45 +73,46 @@ function populateNavBarMenu() {
   mainNavbarList.append(fragment);
 }
 
-function activateClasses(allAnchors, Sections) {
-  allAnchors.classList.remove("menu__link");
-  allAnchors.classList.add("active");
-  Sections.classList.add("your-active-class");
-}
-
-function deactivateClasses(allAnchors, Sections) {
-  allAnchors.classList.remove("active");
-  allAnchors.classList.add("menu__link");
-  Sections.classList.remove("your-active-class");
-}
-
-function setClassesName(allAnchors, index) {
-  for (let i = 0; i < allAnchors.length; i++) {
-    if (i === index) {
-      activateClasses(allAnchors[i], Sections[i]);
-    } else {
-      deactivateClasses(allAnchors[i], Sections[i]);
-    }
-  }
-}
-
+/**
+ * @description Highlighted anchor when click on it and view his section
+ */
 function activeNavbarHighlighted() {
   const allAnchors = document.querySelectorAll("a");
   activateClasses(allAnchors[0], Sections[0]);
   for (let i = 0; i < allAnchors.length; i++) {
     if (allAnchors[i].innerText === Sections[i].id) {
       allAnchors[i].addEventListener("click", function (event) {
+        enableScroll = false;
         event.preventDefault();
         setClassesName(allAnchors, i);
         Sections[i].scrollIntoView();
-        //allAnchors[i].href = `#${Sections[i].id}`;
+        enableScroll = true;
       });
     }
   }
 }
 
+/**
+ * @description Active the anchor when his section in the viewport
+ */
+function activeHisAnchor() {
+  const anchors = document.querySelectorAll("a");
+  window.addEventListener("scroll", function () {
+    if (enableScroll) {
+      for (let i = 0; i < anchors.length; i++) {
+        if (sectionInViewport(Sections[i])) {
+          setClassesName(anchors, i);
+        }
+      }
+    }
+  });
+}
+
+/**
+ * @description show button (up) when scrolling and scroll to top when click on it
+ */
 function scrollToTop() {
-  var upButton = document.getElementById("upButton");
+  const upButton = document.getElementById("upButton");
   window.addEventListener("scroll", function () {
     if (this.window.scrollY > 50) {
       upButton.style.display = "block";
@@ -122,44 +125,34 @@ function scrollToTop() {
   });
 }
 
+/**
+ * @description hide the navBar when no scroll event
+ */
 function hideNavigationBar() {
   let timer = null;
   setTimeout(function () {
-    mainHeader.style.top = "-50px";
+    mainHeader.classList.add("hide");
   }, 2000);
   window.addEventListener("scroll", function () {
-    mainHeader.style.top = "0";
+    mainHeader.classList.remove("hide");
     if (timer !== null) {
       clearTimeout(timer);
     }
     timer = setTimeout(function () {
-      mainHeader.style.top = "-50px";
+      mainHeader.classList.add("hide");
     }, 2000);
   });
 }
 
-function sectionInViewport(section) {
-  const rect = section.getBoundingClientRect();
-  return rect.top >= 0 && rect.bottom <= window.innerHeight;
-}
-
-function activeHisAnchor() {
-  const anchors = document.querySelectorAll("a");
-  window.addEventListener("scroll", function () {
-    for (let i = 0; i < anchors.length; i++) {
-      if (sectionInViewport(Sections[i])) {
-        setClassesName(anchors, i);
-      }
-    }
-  });
-}
-
+/**
+ * @description Make sections collapsible when pressing (...more) they open and when pressing (...lower) they close
+ */
 function collapsing() {
   const collapse = document.getElementsByClassName("collapsible");
   for (let i = 0; i < collapse.length; i++) {
     collapse[i].addEventListener("click", function () {
       collapse[i].classList.add("activeCollapsible");
-      var content = collapse[i].nextElementSibling;
+      const content = collapse[i].nextElementSibling;
       if (content.style.maxHeight) {
         content.style.maxHeight = null;
         collapse[i].classList.remove("activeCollapsible");
@@ -172,8 +165,8 @@ function collapsing() {
 }
 
 populateNavBarMenu();
+activeHisAnchor();
 activeNavbarHighlighted();
 scrollToTop();
 hideNavigationBar();
-activeHisAnchor();
 collapsing();
